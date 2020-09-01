909492f30d022df82c42fbdb5d8bd93a02354187
Emmanuel Eboh <ecolejnr007@gmail.com>
2020-09-01 15:31:34 +0100

    Added a function to prevent simultaneous display of the same cities

--- app.js
+++ app.js
@@ -14,8 +14,37 @@ baseUrl:"https://api.openweathermap.org/data/2.5/weather?"
 // stopping the form from submitting by using preventDefault and then grabbing the user input in the search field
 form.addEventListener("submit", e => {
   e.preventDefault();
-  
   const inputVal = input.value;
+
+//   this code is to prevent the display of the same results(same city)
+const resultItems = showResults.querySelectorAll('.bottom-side .city');
+const resultItemsArray = Array.from(resultItems);
+
+if(resultItemsArray.length > 0){
+    const filteredArray = resultItemsArray.filter(el => {
+        let content = "";
+        if(inputVal.includes(',')){
+            if(inputVal.split(',')[1].length > 2){
+                inputVal = inputVal.split(',')[0];
+                content = el
+                .querySelector('.cityName span')
+                .textContent.toLowerCase();
+            }else {
+                content = el.querySelector('.cityName').dataset.name.toLowerCase();
+            }
+        }else {
+            content = el.querySelector('.cityName span').textContent.toLowerCase();
+        }
+        return content == inputVal.toLowerCase();
+    });
+    if(filteredArray.length > 0){
+        msg.textContent = `The weather for ${filteredArray[0].querySelector('.cityName span').textContent} has been brought out already.... Otherwise you can be more specific with your search by providing the country code as well.`;
+        // 
+        form.reset();
+        input.focus();
+        return;
+    }
+}
 
   //using fetchapi method to send a request to the api
 //    fetch(url)
@@ -29,6 +58,7 @@ form.addEventListener("submit", e => {
   const url = `${api.baseUrl}q=${inputVal}&appid=${api.key}&units=metric`;
   console.log(url)
 
+//   the icon code holds the weather info of the searched city
   fetch(url)
     .then(response => response.json()) 
     .then(data => {
@@ -40,19 +70,21 @@ form.addEventListener("submit", e => {
       const li = document.createElement("li");
       li.classList.add("city");
       const markup = `
-        <h2 class="city-name" data-name="${name},${sys.country}">
+        <h2 class="cityName" dataName="${name},${sys.country}">
           <span>${name}</span>
           <sup>${sys.country}</sup>
         </h2>
-        <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+        <div class="cityTemp">${Math.round(main.temp)}<sup>°C</sup></div>
         <figure>
-          <img class="city-icon" src=${icon} alt=${weather[0]["main"]}>
-          <figcaption>${weather[0]["description"]}<br/>Feels like
-          ${main.feels_like}</figcaption>
+          <img class="cityIcon" src=${icon} alt=${weather[0]["main"]}>
+          <figcaption>${weather[0]["description"]}<br/><b>Feels like
+          ${main.feels_like}</b></figcaption>
         </figure>
       `;
+      
       li.innerHTML = markup;
       showResults.appendChild(li);
+      
     })
     .catch(() => {
       msg.textContent = "Please enter a valid city";
